fac79a8fea4713d4f918dae1c2d43e5c87a75f2c
Niklas Dahlqvist <dalkmania@gmail.com>
2019-10-26 19:45:59 -0700

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# React SID Player
-
-A simple HTML5 C64 SID Player built with ReactJS, Redux, and CSS3.
-
-Github Pages Repo for the Demo: https://dalkmania.github.io/react-sid-player
---
+++ precache-manifest.d4b9805d15ef3961ceb3eb1663d84e52.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2697ddc14921a34817583f684fe8c09d",
+    "url": "/react-sid-player/index.html"
+  },
+  {
+    "revision": "6dad6941be06858f430d",
+    "url": "/react-sid-player/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "a9a7028c2f170ced32af",
+    "url": "/react-sid-player/static/css/main.f3a0b4b3.chunk.css"
+  },
+  {
+    "revision": "6dad6941be06858f430d",
+    "url": "/react-sid-player/static/js/2.7b532a75.chunk.js"
+  },
+  {
+    "revision": "a9a7028c2f170ced32af",
+    "url": "/react-sid-player/static/js/main.e81d1939.chunk.js"
+  },
+  {
+    "revision": "926123b4a7ed873c5f48",
+    "url": "/react-sid-player/static/js/runtime~main.82997534.js"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/react-sid-player/static/media/bars-solid.b3f49afd.svg"
+  },
+  {
+    "revision": "6c862c3f82cc746773582a46cc7b8d3a",
+    "url": "/react-sid-player/static/media/body-background.6c862c3f.jpg"
+  },
+  {
+    "revision": "16ba26b956ed4427b5be8382b39b52bd",
+    "url": "/react-sid-player/static/media/ln1_cover.16ba26b9.jpg"
+  },
+  {
+    "revision": "4ff90d4c9ced8e8e63c95b30b397635f",
+    "url": "/react-sid-player/static/media/ln2_cover.4ff90d4c.jpg"
+  },
+  {
+    "revision": "15905d93adc66bafa5fc958efca95489",
+    "url": "/react-sid-player/static/media/ln3_cover.15905d93.jpg"
+  },
+  {
+    "revision": "2ea4b80f06fc6f7fe06756a305b58d14",
+    "url": "/react-sid-player/static/media/lnrmx_cover.2ea4b80f.jpg"
+  },
+  {
+    "revision": "93c1a16bb17baa502133a0c51bb6726b",
+    "url": "/react-sid-player/static/media/pause-solid.93c1a16b.svg"
+  },
+  {
+    "revision": "f79fe6ae99a50939f12f755851398cf9",
+    "url": "/react-sid-player/static/media/play-solid.f79fe6ae.svg"
+  },
+  {
+    "revision": "2d81914f8f5939d0afe8d9c98d81c746",
+    "url": "/react-sid-player/static/media/step-backward-solid.2d81914f.svg"
+  },
+  {
+    "revision": "d34f2bf432b261243321aa734f4915f9",
+    "url": "/react-sid-player/static/media/step-forward-solid.d34f2bf4.svg"
+  }
+]);
